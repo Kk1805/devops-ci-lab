5da7c6dde4a46b7ac894a30717ab1c3502f1a629
Kk1805 <kiruthikanagaraj1805@gmail.com>
2025-07-14 01:46:00 +0530

    Triggered build from Git change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Kiruthika!");
         System.out.println("CI Trigger Test!");
+        System.out.println("Build triggered from Git!");
     }
 }
